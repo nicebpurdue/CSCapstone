2b982dd32cc1890813a1b0b1aac7486e4b172fd2
Brandon Nice <niceb@purdue.edu>
2016-11-13 13:33:28 -0500

    Added CommentsApp

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/u92/niceb/CS390/python/step0-2/CSCapstone/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- settings.py
+++ settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'CompaniesApp',
     'AuthenticationApp',
     'GroupsApp',
-    'UniversitiesApp',
-    'CommentsApp',
+	'UniversitiesApp',
 ]
 
 MIDDLEWARE = [
